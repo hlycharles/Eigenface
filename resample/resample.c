3a47aef134100e998bd7f05518bc3431b089cad5
Luyao Hou <hly.charles@gmail.com>
2017-12-12 01:57:30 -0500

    remove pragmas

--- resample/resample.c
+++ resample/resample.c
@@ -16,7 +16,6 @@ void resample(int sr, int sc, int src[], int dest[]) {
     double currentVScale = vScale;
     int desti = 0;
     while (i < sr && desti < dimension) {
-// #pragma HLS LOOP_TRIPCOUNT min=20 max=20
         int vStride = (int)floor(currentVScale);
         if (desti == dimension - 1) {
             vStride = sr - i;
@@ -25,16 +24,13 @@ void resample(int sr, int sc, int src[], int dest[]) {
         double currentHScale = hScale;
         int destj = 0;
         while (j < sc && destj < dimension) {
-// #pragma HLS LOOP_TRIPCOUNT min=20 max=20
             int hStride = (int)floor(currentHScale);
             if (destj == dimension - 1) {
                 hStride = sc - j;
             }
             int currentSum = 0;
             for (int k = i; k < i + vStride && k < sr; k++) {
-// #pragma HLS LOOP_TRIPCOUNT min=1 max=9
                 for (int l = j; l < j + hStride && l < sc; l++) {
-// #pragma HLS LOOP_TRIPCOUNT min=1 max=12
                     currentSum += src[k * sc + l];
                 }
             }
